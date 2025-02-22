1e54c29dd475df070523e1291825cff025fd3c70
Alistair <alistair.brock@outlook.com>
2025-02-22 15:45:04 +0000

    Basic login front end

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { AccountsService } from '../_service/accounts.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 
 export class NavComponent {
+
+  model: any = {};
+  private accountService = inject(AccountsService);
+  loggedin = false;
+
+  login()
+  {
+    this.accountService.login(this.model).subscribe({
+      next: response => 
+      {
+        console.log(response);
+        this.loggedin = true;
+      },
+      error: error => console.log(error)
+    });
+    console.log(this.model)
+  }
+
+  logout()
+  {
+    this.loggedin = false;
+  }
 
 }
